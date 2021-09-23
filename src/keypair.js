f930bd38baf0002598702a815f48bd6e57d82419
wyuinche <wyuinche.mido@gmail.com>
2021-09-23 16:01:27 +0900

    add license header

--- src/keypair.js
+++ src/keypair.js
@@ -1,3 +1,20 @@
+/* 
+Copyright (C) 2021 ChainSafe Systems
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 const Web3 = require('web3');
 const web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/af83f335b6cb4b7cb08ec35a723d31e8'));
 const ec = new require('elliptic').ec('secp256k1');
